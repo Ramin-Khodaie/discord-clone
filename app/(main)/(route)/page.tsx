88cea5a87c7ca9b75bc2f66c89b15c03b0919cf6
RaminKh <kh.ramin69@gmail.com>
2023-11-08 11:11:04 +0330

    feat: adding theme

--- app/(main)/(route)/page.tsx
+++ app/(main)/(route)/page.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-9">
       <p className="text-2xl font-bold text-indigo-400">
-        This is a protected rout
+        <UserButton afterSignOutUrl="/" />
       </p>
     </div>
   );
 }
-
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -17,10 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
 }
-
