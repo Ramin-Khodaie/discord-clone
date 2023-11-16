c5ca441bfbd54174be16fcea40859303814519ff
Ramin Khodaie <kh.ramin69@gmail.com>
2023-11-16 13:02:26 +0330

    feat: initial-modal

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import InitialModal from "@/components/modals/initial-modal";
 
 async function SetupPage() {
   const profile = await initialProfile();
@@ -16,8 +17,9 @@ async function SetupPage() {
     },
   });
 
-  if(server) return redirect(`/server/${server.id}`)
-  return <div>Create a Server</div>;
+  if (server) return redirect(`/server/${server.id}`);
+
+  return <InitialModal />;
 }
 
 export default SetupPage;
