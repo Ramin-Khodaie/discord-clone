c62ca7ab0cedcb5b3f4f0ef784ff28b775927546
RaminKh <kh.ramin69@gmail.com>
2024-04-02 21:48:12 +0330

    feat(member): member management

--- components/modals/member-modal.tsx
+++ components/modals/member-modal.tsx
@@ -1,4 +1,17 @@
 "use client";
+import { useState } from "react";
+
+import { ServerWithMembersWithProfiles } from "@/types";
+import {
+  Check,
+  Gavel,
+  Loader,
+  MoreVertical,
+  Shield,
+  ShieldAlert,
+  ShieldCheck,
+  ShieldQuestion,
+} from "lucide-react";
 
 import {
   Dialog,
@@ -10,33 +23,109 @@ import {
 import { useModal } from "@/hooks/use-modal-store";
 import { useOrigin } from "@/hooks/use-origin";
 
-import { ServerWithMembersWithProfiles } from "@/types";
 import { UserAvatar } from "@/components/user-avatar";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MemberRole } from "@prisma/client";
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+};
 
 const MemberModal = () => {
   const { isOpen, onOpen, data, onClose, type } = useModal();
+  const [loadingId, setLoadingId] = useState("");
+
   const origin = useOrigin();
 
   const { server } = data as { server: ServerWithMembersWithProfiles };
 
+  const onRoleChange = async (memberId: string, role: MemberRole) =>{
+    
+  }
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
             Manage Member
-          </DialogTitle>          
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} Members
+          </DialogDescription>
         </DialogHeader>
-        <DialogDescription>{server?.members?.length} Members</DialogDescription>
-        <ScrollArea className="mt-8 max-h">{
-          server.members?.map((member: any)=>(
-            <div key={member?.id} className="flex items-center gap-x-2 mb-6">
-
-              <UserAvatar src={member?.profile?.imageUrl}/>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server.members?.map((member) => (
+            <div key={member?.id} className="flex items-center mb-6">
+              <UserAvatar src={member?.profile?.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div
+                  className={"text-sm font-semibold flex items-center gap-x-1"}
+                >
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
+              </div>
+              {server.profileId !== member.profileId &&
+                loadingId !== member.id && (
+                  <div className="ml-auto">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <MoreVertical className="h-4 w-4 text-zinc-500" />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent side="left">
+                        <DropdownMenuSub>
+                          <DropdownMenuSubTrigger className="flex items-center">
+                            <ShieldQuestion className="h-4 w-4 mr-2" />
+                            <span>Role</span>
+                          </DropdownMenuSubTrigger>
+                          <DropdownMenuSeparator>
+                            <DropdownMenuSubContent>
+                              <DropdownMenuItem>
+                                <Shield className="h-4 w-4 mr-2" />
+                                Guest
+                                {member.role === "GUEST" && (
+                                  <Check className="h-4 w-4 ml-auto" />
+                                )}
+                              </DropdownMenuItem>
+                              <DropdownMenuItem>
+                                <ShieldCheck className="h-4 w-4 mr-2" />
+                                Moderator
+                                {member.role === "MODERATOR" && (
+                                  <Check className="h-4 w-4 ml-auto" />
+                                )}
+                              </DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                          </DropdownMenuSeparator>
+                        </DropdownMenuSub>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                          <Gavel className="h-4 w-4 mr-2" />
+                          Kick
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                )}
+              {loadingId === member.id && (
+                <Loader className="animate-spin text-zinc-500 ml-auto w-4 h-4" />
+              )}
             </div>
-          ))
-        }</ScrollArea>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
